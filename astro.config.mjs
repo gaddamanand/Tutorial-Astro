761dc838e2b86a9064e9964e9f04643aa45b76bb
anandgaddam <gaddamanand@hotmail.com>
2024-04-06 20:51:33 +0700

    Adding Components, Header, Themicon, RSS, Tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://tutorial-astro-9w78yh7a9-gaddamanand.vercel.app",
+  integrations: [preact()]
+});
